276f7f3f64c0981e4af97d2ae781779acb57b0d4
sunilkondeti234 <sunilkondeti234@gmail.com>
2025-06-22 00:41:48 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // for all components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
